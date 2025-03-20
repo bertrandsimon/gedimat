ff0d35d726da6cf4c27f8fb0469d548e7b51efdf
bertrandsimon <trandber@hotmail.com>
2025-03-20 17:00:20 +0100

    All galleries .js done

--- data/gallery-exterieur.js
+++ data/gallery-exterieur.js
@@ -2,89 +2,89 @@ const data = [
   // --- Column #1 ---
   {
     "id": "1",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-design.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-design-2311.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-design-epure-un-espace-minimaliste-aux-tons-neutres-2311.htm"
   },
   {
     "id": "2",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-noir.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-noir-2314.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-noir-et-blanc-une-terrasse-chic-et-confortable-2314.htm"
   },
   {
     "id": "3",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-evasion.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-evasion-2361.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-evasion-exotique-une-terrasse-vegetale-et-cosy-pour-un-depaysement-garanti-2361.htm"
   },
   {
     "id": "4",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-design-epure-2023.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-design-epure-2023-2455.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-design-epure-terrasse-amenagee-en-patio-2455.htm"
   },
   {
     "id": "5",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2022.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2022-2458.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-classique-chic-esprit-guinguette-chic-2458.htm"
   },
   {
     "id": "6",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-design-epure-2022-2.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-design-epure-2022-2461.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-design-epure-espace-detente-ombrage-2461.htm"
   },
   {
     "id": "7",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2022-1.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2022-2464.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-classique-chic-terrasse-et-piscine-2464.htm"
   },
   {
     "id": "8",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-loft-indus-2021.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-loft-indus-2021-2467.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-loft-indus-patio-urbain-2467.htm"
   },
 
   // --- Column #2 ---
   {
     "id": "9",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-retro.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-retro-2312.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-retro-cool-un-espace-vintage-qui-swingue-2312.htm"
   },
   {
     "id": "10",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-classique.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-classique-2359.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-classique-chic-une-terrasse-entre-modernite-et-sobriete-2359.htm"
   },
   {
     "id": "11",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2023.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2023-2453.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-classique-chic-sur-la-cote-fleurie-2453.htm"
   },
   {
     "id": "12",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2023-2.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-classique-chic-2023-2456.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-classique-chic-maison-de-caractere-2456.htm"
   },
   {
     "id": "13",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-evasion-exotique-2022.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-evasion-exotique-2022-2459.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-evasion-exotique-terrasse-decaissee-originale-2459.htm"
   },
   {
     "id": "14",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-loft-indus-2.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-loft-indus-2-2462.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-loft-indus-deliberement-industriel-2462.htm"
   },
   {
     "id": "15",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2022.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2022-2465.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-scandi-boheme-espace-nordique-2465.htm"
   },
   {
     "id": "16",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2021.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2021-2462.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-loft-indus-deliberement-industriel-2462.htm"
   },
   {
     "id": "17",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2021.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2021-2468.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-scandi-boheme-terrasse-champetre-2468.htm"
   },
  
@@ -92,32 +92,32 @@ const data = [
   // --- Column #3 ---
   {
     "id": "18",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-scandi.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-scandi-2319.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-scandinave-boheme-l-esprit-ibiza-chez-soi-2319.htm"
   },
   {
     "id": "19",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-loft.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/galerie/terrasses/terrasse-loft-2360.jpg",
     "link": "https://www.gedimat.fr/article-terrasse-loft-indus-une-terrasse-qui-a-du-caractere-2360.htm"
   },
   {
     "id": "20",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2023.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-scandi-boheme-2023-2454.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-scandi-boheme-nordique-cocooning-2454.htm"
   },
   {
     "id": "21",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-noir-et-blanc-2022.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-noir-et-blanc-2022-2469.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-noir-et-blanc-intemportel-aux-contrastes-marques-2469.htm"
   },
   {
     "id": "22",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-design-epure-2022.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-design-epure-2022-2364.jpg",
     "link": "https://www.gedimat.fr/article-salle-de-bains-scandi-boheme-un-mariage-harmonieux-des-tons-sable-et-terracotta-2364.htm"
   },
   {
     "id": "23",
-    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-retro-cool-2022.jpg",
+    "img_url": "https://uploads.gedimat.fr/CMS/images/tendances2024/pieces/exterieur-retro-cool-2022-2463.jpg",
     "link": "https://www.gedimat.fr/article-exterieur-retro-cool-cuisine-d-ete-2463.htm"
   }
 ];
